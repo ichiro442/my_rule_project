15305244eecdbd65f38929104a0644f3a970b553
ichiro <ichiro.and.juliet.and@gmail.com>
2020-10-19 20:58:30 +0900

    title変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>自分ルール</title>
+        <title>自分クエスト</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
