847fc87bddcf39484d52c691f53c54593c9d0566
ichiro <ichiro.and.juliet.and@gmail.com>
2020-10-23 22:05:32 +0900

    コメントアウトで日本語訳を書いた「リレーション  一対多とか一対一とかのことだね」など

--- app/Behavior.php
+++ app/Behavior.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+// リレーション  一対多とか一対一とかのことだね
 
 class Behavior extends Model
 {
--- app/Level_standard.php
+++ app/Level_standard.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+// リレーション  一対多とか一対一とかのことだね
 
 class Level_standard extends Model
 {
